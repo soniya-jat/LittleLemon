c7f6f935c6f7291ffb9d2ecd1546f7fece89bbaf
soniya-jat <2419202843@onlinesmu.edu.in>
2026-07-04 13:15:41 +0530

    Update database settings to use MySQL and add Booking and Menu models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+	id = models.AutoField(primary_key=True, db_column='ID')
+	name = models.CharField(max_length=255, db_column='Name')
+	no_of_guests = models.IntegerField(db_column='No_of_guests')
+	booking_date = models.DateTimeField(db_column='BookingDate')
+
+class Menu(models.Model):
+	id = models.AutoField(primary_key=True, db_column='ID')
+	name = models.CharField(max_length=255, db_column='Name')
+	price = models.DecimalField(max_digits=10, decimal_places=2, db_column='Price')
+	inventory = models.IntegerField(db_column='Inventory')
